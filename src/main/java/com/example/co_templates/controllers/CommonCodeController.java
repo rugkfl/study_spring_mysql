8144d7836b5796f1509dd5973de7b4a002efccd3
rugkfl <kimgyoungha61@gmail.com>
2024-04-17 05:33:28 +0000

    update

--- src/main/java/com/example/co_templates/controllers/CommonCodeController.java
+++ src/main/java/com/example/co_templates/controllers/CommonCodeController.java
@@ -1,7 +1,9 @@
 package com.example.co_templates.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,20 +12,32 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.co_templates.services.CommonCodeService;
 
-import java.util.ArrayList;
-
-import java.util.HashMap;
-
 @Controller
 public class CommonCodeController {
 
     @Autowired
     CommonCodeService commonCodeService;
 
-    @GetMapping("/commonCode/list")
+    // @GetMapping("/commonCode/list")
     public ModelAndView list(ModelAndView modelAndView
-                        ,@RequestParam HashMap<String, Object> dataMap
-                        ,@RequestParam(name = "deleteIds", required = false ) ArrayList<String> deleteIds) {
+                    , @RequestParam HashMap<String, Object> dataMap) {
+        ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
+        // Call Service with Pure Java
+        // CommonCodeService commonCodeService = new CommonCodeService();
+        itemList = commonCodeService.list(1);
+
+        String viewPath = "/WEB-INF/views/commoncode/list.jsp";
+        modelAndView.setViewName(viewPath);
+        modelAndView.addObject("itemList", itemList);
+        modelAndView.addObject("dataMap", dataMap);
+
+        return modelAndView;
+    }    
+
+    @GetMapping("/commonCode/list")
+    public ModelAndView listWithDB(ModelAndView modelAndView
+                    , @RequestParam HashMap<String, Object> dataMap
+                    , @RequestParam(name = "deleteIds", required = false) ArrayList<String> deleteIds) {
         ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
         // Call Service with Pure Java
         // CommonCodeService commonCodeService = new CommonCodeService();
@@ -35,14 +49,14 @@ public class CommonCodeController {
         modelAndView.addObject("dataMap", dataMap);
 
         return modelAndView;
-    }
-
-    // paginations
+    }  
+    
     @GetMapping("/commonCode/list_pagination")
-    public ModelAndView listpagination(ModelAndView modelAndView
-                        ,@RequestParam HashMap<String, Object> dataMap
-                        ,@RequestParam(name = "deleteIds", required = false ) ArrayList<String> deleteIds) {
-        Object result = commonCodeService.selectSearchWithPagination(dataMap);
+    public ModelAndView listPagination(ModelAndView modelAndView
+                    , @RequestParam HashMap<String, Object> dataMap
+                    , @RequestParam(name = "deleteIds", required = false) ArrayList<String> deleteIds) {
+        dataMap.put("deleteIds", deleteIds);
+        Object result = commonCodeService.selectSearchWithPaginationAndDeletes(dataMap);
 
         String viewPath = "/WEB-INF/views/commoncode/list_pagination.jsp";
         modelAndView.setViewName(viewPath);
@@ -50,8 +64,7 @@ public class CommonCodeController {
         modelAndView.addObject("dataMap", dataMap);
 
         return modelAndView;
-    }
-
+    }    
+    
 
 }
-
--- src/main/java/com/example/co_templates/services/CommonCodeService.java
+++ src/main/java/com/example/co_templates/services/CommonCodeService.java
@@ -1,8 +1,8 @@
 package com.example.co_templates.services;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +19,9 @@ public class CommonCodeService {
     @Autowired
     Commons commons;
 
+    // @Autowired
+    // Paginations paginations;
+
     public Object selectMany(HashMap<String, Object> dataMap) {
         // 여러개 가져오기
         String sqlMapId = "CommonCode.selectBysearch";
@@ -27,16 +30,17 @@ public class CommonCodeService {
     }
 
     public Object selectTotal(Map dataMap) {
-        String sqlMapId = "CommonCode.selectTotal"; // 전체 개수만 가져옴
+        String sqlMapId = "CommonCode.selectTotal"; // 전체 개수 가져옴
+
         Object result = shareDao.getOne(sqlMapId, dataMap);
         return result;
-    }
+    }    
 
     public Map selectSearchWithPagination(Map dataMap) {
         // 페이지 형성 위한 계산
         int totalCount = (int) this.selectTotal(dataMap);
-
-        int currentPage = 1; // 값이 안들어오면 1페이지
+        
+        int currentPage = 1;
         if(dataMap.get("currentPage") != null) {
             currentPage = Integer.parseInt((String)dataMap.get("currentPage"));    // from client in param
         }
@@ -54,15 +58,52 @@ public class CommonCodeService {
         return result;
     }
 
-    public void callDao(HashMap<String, Object> dataMap){
+    public Object deleteWithIn(Map dataMap){
+        String sqlMapId = "CommonCode.deletewithin";
+        Object count = shareDao.delete(sqlMapId, dataMap);
+        return count;
+    }
 
+    public Map selectSearchWithPaginationAndDeletes(Map dataMap) {
+        // delete
+        if (dataMap.get("deleteIds") != null){
+            Object count = this.deleteWithIn(dataMap);
+        }
+
+        // 페이지 형성 위한 계산
+        int totalCount = (int) this.selectTotal(dataMap);
+        
+        int currentPage = 1;
+        if(dataMap.get("currentPage") != null) {
+            currentPage = Integer.parseInt((String)dataMap.get("currentPage"));    // from client in param
+        }
+
+        Paginations paginations = new Paginations(totalCount, currentPage);
+        HashMap result = new HashMap<>();
+        result.put("paginations", paginations); // 페이지에 대한 정보
+
+        // page record 수
+        String sqlMapId = "CommonCode.selectSearchWithPagination";
+        dataMap.put("pageScale", paginations.getPageScale());
+        dataMap.put("pageBegin", paginations.getPageBegin());
+        
+        result.put("resultList", shareDao.getList(sqlMapId, dataMap)); // 표현된 레코드 정보
+        return result;
+    }
+
+    public void callDao(HashMap<String, Object> dataMap) {
+
+        this.selectMany(dataMap);
         // 한개만 가져오기
         String sqlMapId = "CommonCode.selectByUID";
         dataMap.put("PK_UNIQUE", "CC002");
         Object one = shareDao.getOne(sqlMapId, dataMap);
 
-        String pk_unique = commons.getUniqueSequence();
         sqlMapId = "CommonCode.insert";
+        // (`PK_UNIQUE`, `CODE_NAME`, `DESCRIPTION`, `PARENT_UNIQUE`)
+        // VALUES
+        // ('CC017', 'test_insert', 'test insert', '');
+        String pk_unique = commons.getUniqueSequence();
         dataMap.put("PK_UNIQUE", pk_unique);
         dataMap.put("CODE_NAME", "test_insert");
         dataMap.put("DESCRIPTION", "test insert");
@@ -77,26 +118,25 @@ public class CommonCodeService {
         sqlMapId = "CommonCode.delete";
         dataMap.put("PK_UNIQUE", pk_unique);
         Object delete = shareDao.delete(sqlMapId, dataMap);
-
         return;
     }
 
-    public HashMap<String, Object> mixed(Integer pageNumber, Integer pkid){
+    public HashMap<String, Object> mixed(Integer pageNumber, Integer pkid) {
         HashMap<String, Object> resultMap = new HashMap<>();
         resultMap.put("list", this.list(pageNumber));
         resultMap.put("view", this.view(pkid));
         return resultMap;
     }
 
-    public ArrayList<HashMap<String, Object>> list(Integer pageNumber){
+    public ArrayList<HashMap<String, Object>> list(Integer pageNumber) {
         ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
- 
+
         HashMap<String, Object> item = new HashMap<>();
         item.put("PK_ID", 1);
         item.put("FK_ID", 10);
         item.put("NAME", "Excalibur");
         itemList.add(item);
-    
+
         item = new HashMap<>();
         item.put("PK_ID", 2);
         item.put("FK_ID", 20);
@@ -106,7 +146,7 @@ public class CommonCodeService {
         return itemList;
     }
 
-    public HashMap<String, Object> view(Integer pkId){
+    public HashMap<String, Object> view(Integer pkId) {
         HashMap<String, Object> itemDetails = new HashMap<>();
 
         String fkId = "FK_0382";
